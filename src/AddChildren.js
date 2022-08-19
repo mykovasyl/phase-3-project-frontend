b2d62bbd4866a0471e72dbe54e756530092c4de0
Mykola Vasyl'yev <mykovasyl@gmail.com>
2022-08-19 13:24:08 -0400

    update delete, post requests, map over children to create table, remove div from Child

--- src/AddChildren.js
+++ src/AddChildren.js
@@ -15,26 +15,24 @@ function AddChildren({ children, setChildren }) {
       headers: {
         "Content-Type": "application/json",
       },
-      body: JSON.stringify(childName),
+      body: JSON.stringify({ name: childName }),
     })
       .then((resp) => resp.json())
       .then((newChild) => {
-        setChildren([...newChild, children]);
+        setChildren([...children, newChild]);
+        setChildName("");
       });
-  }
-
-  function handleDelete(id) {
-    const newChildren = children.filter((child) => child.id !== id);
-    fetch("http://localhost:9292/children/:id", {
-      method: "DELETE",
-    });
   }
 
   let childrenRows = children.map((child) => {
     return (
-      <tr>
-        <Child id={child.id} name={child.name} handleDelete={handleDelete} />
-      </tr>
+      <Child
+        key={child.id}
+        id={child.id}
+        name={child.name}
+        children={children}
+        setChildren={setChildren}
+      />
     );
   });
 
@@ -44,15 +42,22 @@ function AddChildren({ children, setChildren }) {
       <br></br>
       <p>Type your child's name and click submit</p>
       <form onSubmit={handleNewChild}>
-        <input type="text" onChange={handleNameInput} />
+        <input type="text" value={childName} onChange={handleNameInput} />
         <button type="submit">Submit</button>
       </form>
       <table>
-        <tr>
-          <td>Name</td>
-          <td>Edit/Delete</td>
-        </tr>
-        {childrenRows}
+        <thead>
+          <tr>
+            <td>Current Children</td>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td>Name</td>
+            <td>Edit/Delete</td>
+          </tr>
+          {childrenRows}
+        </tbody>
       </table>
     </div>
   );
--- src/Child.js
+++ src/Child.js
@@ -1,16 +1,24 @@
 import React from "react";
 import { HiTrash } from "react-icons/hi";
 
-function Child({ id, name, handleDelete }) {
+function Child({ id, name, children, setChildren }) {
+  function handleDelete() {
+    const newChildren = children.filter((child) => child.id !== id);
+    fetch(`http://localhost:9292/children/${id}`, {
+      method: "DELETE",
+    });
+    setChildren(newChildren);
+  }
+
   return (
-    <div>
+    <tr>
       <td>{name}</td>
       <td>
-        <button onClick={handleDelete(id)}>
+        <button onClick={handleDelete}>
           <HiTrash />
         </button>
       </td>
-    </div>
+    </tr>
   );
 }
 
