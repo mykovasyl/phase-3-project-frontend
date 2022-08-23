f3916639aeef5ff7f20e29a07ecee21817e2359f
Mykola Vasyl'yev <mykovasyl@gmail.com>
2022-08-23 17:50:06 -0400

    change page names

--- src/NavBar.js
+++ src/NavBar.js
@@ -5,9 +5,9 @@ function NavBar() {
   return (
     <nav id="navigation">
       <Link to="/">Home</Link>
-      <Link to="/chorelist">Chore List</Link>
+      <Link to="/chorelist">Chore Table</Link>
       <Link to="/assignchores">Assign Chores</Link>
-      <Link to="/addchildren">Add Children</Link>
+      <Link to="/addchildren">Children</Link>
     </nav>
   );
 }
