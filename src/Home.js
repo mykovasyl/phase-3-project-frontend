99f8f3d2b5d3a5069a5e0df5df0e024bf8a6cca7
Mykola Vasyl'yev <mykovasyl@gmail.com>
2022-08-23 09:52:55 -0400

    add chorelist link to home component

--- src/Home.js
+++ src/Home.js
@@ -4,7 +4,7 @@ import { Link } from "react-router-dom";
 function Home() {
   return (
     <div>
-      <h1>Welcome to the Chores Game</h1>
+      <h1>Welcome to Chores Gallore!</h1>
       <p>
         This Application was built to encourage children to do their chores
         through a game style level system.
@@ -26,9 +26,7 @@ function Home() {
           <h2>Assign Chores</h2>
         </Link>
       </nav>
-      <p>
-        On this page you'll be able to assign a specific chore to your child.
-      </p>
+      <p>You'll be able to assign a specific chore to your child.</p>
       <p>
         You are able to set the points and due date according to your specific
         needs.
@@ -42,12 +40,18 @@ function Home() {
           <h2>Add Children</h2>
         </Link>
       </nav>
-
       <p>
-        On this page you can add children into the system so they can be
-        assigned chores.
+        Here you can add children to your household so they can be assigned
+        chores.
       </p>
       <p>Every child's points automatically start at 0.</p>
+      <nav>
+        <Link to="/chorelist">
+          <h2>Chore List</h2>
+        </Link>
+      </nav>
+      <p>Two organized tables of all current and completed chores.</p>
+      <p>You can edit and delete chores on this page.</p>
     </div>
   );
 }
